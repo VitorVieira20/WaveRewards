37362421ef396a0c15af3af6ee239ad962d85f04
VitorVieira20 <vitor20vieira2@gmail.com>
2025-12-03 17:08:16 +0000

    fix: fixed chatbot send message in production

--- resources/js/Components/Chatbot/Chatbot.jsx
+++ resources/js/Components/Chatbot/Chatbot.jsx
@@ -92,7 +92,7 @@ export default function Chatbot({ onClose }) {
         setBotIsTyping(true);
 
         try {
-            const response = await fetch('http://localhost:8000/api/chat', {
+            const response = await fetch('/api/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
